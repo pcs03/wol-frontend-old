664fb80d0760b15aea710fe419a0ad1c5abe731b
Paolo Stet <paolostet@gmail.com>
2023-07-01 21:05:23 +0200

    Remove proto

--- make-proto.sh
+++
@@ -1,7 +0,0 @@
-protoc \
-    --plugin="./node_modules/.bin/protoc-gen-ts_proto" \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_out="./src/generated" \
-    src/proto/data.proto
-
-cp ./src/proto/data.proto /work/code/wolBackend/proto/data.proto
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,6 @@
 import './App.css'
-import { AddressBook, Person_PhoneType } from './generated/src/proto/data'
 
 function App() {
-  const myAddressBook: AddressBook = {
-    people: [
-      {
-        name: "Joe Blogs",
-        phones: [
-          {
-            phoneNumber: "0123456789",
-            phoneType: Person_PhoneType.MOBILE,
-          }
-        ]
-      },
-      {
-        name: "Jane Smith",
-        phones: [
-          {
-            phoneNumber: "0987654321",
-            phoneType: Person_PhoneType.HOME,
-          }
-        ]
-      }
-    ]
-  }
-
-  const serialMesasge = AddressBook.encode(myAddressBook).finish();
 
   return (
     <div>
